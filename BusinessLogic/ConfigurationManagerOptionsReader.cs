85bfb09eeaba52436c7ef336e0ac89767472ad2a
tankistqazwsx <alexey.bezruchenko@yandex.ru>
2021-12-24 13:00:08 +0300

    Added command line support

---
+++ BusinessLogic/ConfigurationManagerOptionsReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public static class ConfigurationManagerOptionsReader
+    {
+        public static ProgramTranslationOptions GetArguments(NameValueCollection AppSettings)
+        {
+            ProgramTranslationOptions result = new ProgramTranslationOptions();
+            result.ResourcePath = AppSettings.Get("ResourcePath");
+            result.FromLanguage = AppSettings.Get("FromLanguage");
+            result.ToLanguage = AppSettings.Get("ToLanguage");
+            result.Mode = AppSettings.Get("Mode");
+            result.SkipError429 = Convert.ToBoolean(AppSettings.Get("Skip429"));
+            result.SolutionPath = AppSettings.Get("SolutionPath");
+            result.FileToRefactor = AppSettings.Get("FileToRefactor");
+
+            return result;
+        }
+    }
+}
---
+++ BusinessLogic/ProgramTranslateOptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace BusinessLogic
+{
+    public class ProgramTranslationOptions
+    {
+        [Option("ResourcePath", Required = false, HelpText = "Path to resource file. Should not include file name.")]
+        virtual public string ResourcePath { get; set; }
+
+        [Option("FromLanguage", Required = false, HelpText = "")]
+        virtual public string FromLanguage { get; set; }
+
+        [Option("ToLanguage", Required = false, HelpText = "")]
+        virtual public string ToLanguage { get; set; }
+
+        [Option("Mode", Required = false, HelpText = "")]
+        virtual public string Mode { get; set; }
+
+        [Option("SkipError429", Required = false, HelpText = "")]
+        virtual public bool? SkipError429 { get; set; }
+
+        [Option("SolutionPath", Required = false, HelpText = "")]
+        virtual public string SolutionPath { get; set; }
+
+        [Option("FileToRefactor", Required = false, HelpText = "")]
+        virtual public string FileToRefactor { get; set; }
+
+        public ProgramTranslationOptions fillNullsFrom(ProgramTranslationOptions other)
+        {
+            ResourcePath = ResourcePath ?? other.ResourcePath;
+            FromLanguage = FromLanguage ?? other.FromLanguage;
+            ToLanguage = ToLanguage ?? other.ToLanguage;
+            Mode = Mode ?? other.Mode;
+            SkipError429 = SkipError429 ?? other.SkipError429;
+            SolutionPath = SolutionPath ?? other.SolutionPath;
+            FileToRefactor = FileToRefactor ?? other.FileToRefactor;
+            return this;
+        }
+    }
+}
